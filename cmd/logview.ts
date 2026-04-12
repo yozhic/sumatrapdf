0d18c1fb97cb5db59e42cac43c4a0a3831ed5106
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2026-04-12 22:39:04 +0200

    remove cmd/logview.ts and logview-build.ts

--- cmd/logview.ts
+++
@@ -1,6 +0,0 @@
-import { $ } from "bun";
-import { join } from "node:path";
-
-const dir = join("tools", "logview-web");
-console.log(`running logview-web in ${dir}`);
-await $`go run . -run-dev`.cwd(dir);
